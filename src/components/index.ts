7b329871d06c156e0ce66b5b41a236611def38b2
gpalade <gpalade@totalsoft.ro>
2023-04-11 12:24:42 +0300

    fix tsx, add pagination to components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ import BackToButton from './buttons/BackToButton'
 import UploadButton from './buttons/UploadButton'
 import Typography from './dataDisplay/Typography'
 import ExpandingText from './dataDisplay/ExpandingText'
+import Pagination from './navigation/Pagination'
 import getTheme from './themes/index'
 
-export { getTheme, Button, IconButton, BackToButton, UploadButton, Typography, ExpandingText }
+export { getTheme, Button, IconButton, BackToButton, UploadButton, Typography, ExpandingText, Pagination }
