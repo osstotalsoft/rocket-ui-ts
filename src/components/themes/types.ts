3f13f75caafd5b753b28e7db4c74f7bc088fe4d5
Alexandra Capatina <46926675+alexandra-c@users.noreply.github.com>
2023-04-11 11:06:01 +0300

    infra fixes

--- src/components/themes/types.ts
+++ src/components/themes/types.ts
@@ -17,6 +17,7 @@ import { CSSProperties } from 'react'
 declare module '@mui/material' {
   export interface Theme {
     customShadows: CustomShadows
+    palette: Palette
   }
 
   export interface Palette {
@@ -27,6 +28,7 @@ declare module '@mui/material' {
     black: Partial<PaletteColor>
     link: Partial<PaletteColor>
     default: PaletteColor
+    grey: Color
   }
 
   export interface PaletteOptions {
