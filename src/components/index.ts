4901ed9dcbe7261ca8d1e44ed178ca11a030f5d5
Alexandra Capatina <46926675+alexandra-c@users.noreply.github.com>
2023-05-10 16:09:22 +0300

    Revert "esm imports mappings"
    This reverts commit 35f4e3f35d11cbb395fcbc2b12167df7bfd1dafb.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,46 +1,46 @@
 export * from './types'
 
-export { default as Button } from './buttons/Button/index'
-export * from './buttons/Button/index'
+export { default as Button } from './buttons/Button'
+export * from './buttons/Button'
 
-export { default as IconButton } from './buttons/IconButton/index'
-export * from './buttons/IconButton/index'
+export { default as IconButton } from './buttons/IconButton'
+export * from './buttons/IconButton'
 
-export { default as BackToButton } from './buttons/BackToButton/index'
-export * from './buttons/BackToButton/index'
+export { default as BackToButton } from './buttons/BackToButton'
+export * from './buttons/BackToButton'
 
-export { default as UploadButton } from './buttons/UploadButton/index'
-export * from './buttons/UploadButton/index'
+export { default as UploadButton } from './buttons/UploadButton'
+export * from './buttons/UploadButton'
 
-export { default as Typography } from './dataDisplay/Typography/index'
-export * from './dataDisplay/Typography/index'
+export { default as Typography } from './dataDisplay/Typography'
+export * from './dataDisplay/Typography'
 
-export { default as ExpandingText } from './dataDisplay/ExpandingText/index'
-export * from './dataDisplay/ExpandingText/index'
+export { default as ExpandingText } from './dataDisplay/ExpandingText'
+export * from './dataDisplay/ExpandingText'
 
-export { default as TextField } from './inputs/TextField/index'
-export * from './inputs/TextField/index'
+export { default as TextField } from './inputs/TextField'
+export * from './inputs/TextField'
 
-export { default as PasswordField } from './inputs/PasswordField/index'
-export * from './inputs/PasswordField/index'
+export { default as PasswordField } from './inputs/PasswordField'
+export * from './inputs/PasswordField'
 
-export { default as NavPills } from './navigation/NavPills/index'
-export * from './navigation/NavPills/index'
+export { default as NavPills } from './navigation/NavPills'
+export * from './navigation/NavPills'
 
-export { default as Pagination } from './navigation/Pagination/index'
-export * from './navigation/Pagination/index'
+export { default as Pagination } from './navigation/Pagination'
+export * from './navigation/Pagination'
 
-export { Card, CardActions, CardHeader } from './surfaces/Card/index'
-export * from './surfaces/Card/index'
+export { Card, CardActions, CardHeader } from './surfaces/Card'
+export * from './surfaces/Card'
 
-export { default as CollapseCard } from './surfaces/CollapseCard/index'
-export * from './surfaces/CollapseCard/index'
+export { default as CollapseCard } from './surfaces/CollapseCard'
+export * from './surfaces/CollapseCard'
 
-export { default as StatsCard } from './surfaces/StatsCard/index'
-export * from './surfaces/StatsCard/index'
+export { default as StatsCard } from './surfaces/StatsCard'
+export * from './surfaces/StatsCard'
 
-export { default as Accordion } from './surfaces/Accordion/index'
-export * from './surfaces/Accordion/index'
+export { default as Accordion } from './surfaces/Accordion'
+export * from './surfaces/Accordion'
 
 export { default as getTheme } from './themes/index'
 export * from './themes/index'
