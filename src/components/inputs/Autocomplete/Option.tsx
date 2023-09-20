0375259280f63440949e199eff4ff9900862a110
Costi <cdiaconita@totalsoft.ro>
2023-09-20 11:12:04 +0300

    small fix

--- src/components/inputs/Autocomplete/Option.tsx
+++ src/components/inputs/Autocomplete/Option.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
-import Checkbox from '@mui/material/Checkbox'
-import Tooltip from '@mui/material/Tooltip'
+import { is } from 'ramda'
+import { Checkbox, Tooltip } from '@mui/material'
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
 import CheckBoxIcon from '@mui/icons-material/CheckBox'
 import { Option as BaseOption } from './AutocompleteStyles'
@@ -30,7 +30,7 @@ const Option = ({ optionLabel, createdLabel, selected, withCheckboxes, option, .
     </li>
   ) : (
     <Tooltip title={optionLabel} disableHoverListener={!isOverflow}>
-      <li {...rest} aria-disabled={option?.isDisabled}>
+      <li {...rest} aria-disabled={is(String, option) ? false : option?.isDisabled}>
         <BaseOption ref={optionRef}>
           <Typography>{label}</Typography>
         </BaseOption>
@@ -44,7 +44,7 @@ Option.propTypes = {
   selected: PropTypes.bool,
   withCheckboxes: PropTypes.bool,
   createdLabel: PropTypes.string,
-  option: PropTypes.object
+  option: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
 }
 
 export default Option
