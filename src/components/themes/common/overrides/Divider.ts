ad6d9c64e7932bca767f0fe292299a0ef4d16d94
Alexandra Capatina <46926675+alexandra-c@users.noreply.github.com>
2024-04-10 10:02:47 +0300

    make vertical divider work

--- src/components/themes/common/overrides/Divider.ts
+++ src/components/themes/common/overrides/Divider.ts
@@ -11,8 +11,21 @@ export default function Divider(): CustomComponents {
           borderBottom: 'none',
           margin: '1rem 0px',
           height: '0.0625rem',
+          width: '100%',
           backgroundColor: 'transparent',
           backgroundImage: 'linear-gradient(to right, rgba(52, 71, 103, 0), rgba(52, 71, 103, 0.4), rgba(52, 71, 103, 0))',
+          opacity: '0.45'
+        },
+        vertical: {
+          borderTop: '0px solid rgba(0, 0, 0, 0.12)',
+          borderRight: '0px solid rgba(0, 0, 0, 0.12)',
+          borderLeft: '0px solid rgba(0, 0, 0, 0.12)',
+          borderBottom: 'none',
+          margin: '0px 1rem',
+          width: '0.0625rem',
+          height: '100%',
+          backgroundColor: 'transparent',
+          backgroundImage: 'linear-gradient(rgba(52, 71, 103, 0), rgba(52, 71, 103, 0.4), rgba(52, 71, 103, 0))',
           opacity: '0.45'
         }
       }
