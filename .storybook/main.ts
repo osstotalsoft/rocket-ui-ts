4b5e3e45b88a7f4ecfa42a719899ac006f05a6d0
Andra <andarbuz@gmail.com>
2024-10-31 15:16:24 +0200

    undo rsbuild

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,32 @@
-import { dirname, join } from 'path'
-import type { StorybookConfig } from 'storybook-react-rsbuild'
+import { dirname, join } from "path";
+import type { StorybookConfig } from '@storybook/react-webpack5'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/preview-api')
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/preview-api"),
+    getAbsolutePath("@storybook/addon-webpack5-compiler-swc"),
   ],
   staticDirs: ['../public'],
-  framework: 'storybook-react-rsbuild',
+  framework: '@storybook/react-webpack5',
+  core:{
+    builder:"@storybook/builder-webpack5"
+  },
   typescript: {
     reactDocgen: 'react-docgen',
     check: false
-  }
+  },
+  webpackFinal: async (config: any) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()]
+    return config
+  },
 }
 export default config
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, "package.json")));
 }
