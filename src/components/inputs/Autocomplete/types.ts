b0a5d68d7d0be10febab225873c75a7a8936d751
drosca@totalsoft.ro <drosca@totalsoft.ro>
2024-11-30 08:20:38 +0200

    added the new Autocomplete

--- src/components/inputs/Autocomplete/types.ts
+++ src/components/inputs/Autocomplete/types.ts
@@ -1,17 +1,26 @@
-import { AutocompleteChangeReason, AutocompleteValue, AutocompleteProps as MuiAutocompleteProps } from '@mui/material'
-import { TypographyColor } from '../../dataDisplay/Typography'
+import {
+  AutocompleteChangeDetails,
+  AutocompleteChangeReason,
+  AutocompleteCloseReason,
+  AutocompleteInputChangeReason,
+  AutocompleteOwnerState,
+  AutocompleteRenderInputParams,
+  AutocompleteRenderOptionState,
+  AutocompleteValue,
+  ChipTypeMap,
+  AutocompleteProps as MuiAutocompleteProps
+} from '@mui/material'
 import { TextFieldProps } from '../TextField'
-import { AutocompleteRenderInputParams, AutocompleteRenderGroupParams } from '@mui/material'
 
-export interface OptionProps extends React.HTMLAttributes<HTMLLIElement> {
+export interface OptionProps {
   /**
-   * The string value for a given option
+   * The display value for a given option
    */
-  optionLabel?: string
+  label: React.ReactNode
   /**
-   * Custom label displayed when @creatable is true
+   * The props to apply on the li element.
    */
-  createdLabel?: string
+  liProps: React.HTMLAttributes<HTMLLIElement> & { key: any }
   /**
    *  The selected option(s)
    */
@@ -24,60 +33,61 @@ export interface OptionProps extends React.HTMLAttributes<HTMLLIElement> {
   /**
    * Item option.
    */
-  option?: any
+  option: any
 }
 
-export type LoadOptionsPaginated = (
-  input: string,
-  options: ReadonlyArray<any>,
-  additional?: any
-) => Promise<{ loadedOptions: ReadonlyArray<any>; more: boolean; additional: any }>
+export type LoadOptionsPaginatedResult<T> = { loadedOptions: ReadonlyArray<T>; more: boolean; additional: unknown }
 
-export type LoadOptions = (input: string) => Promise<any>
+export type LoadOptionsPaginated<T> = (
+  input: string,
+  options: ReadonlyArray<T>,
+  additional?: any
+) => Promise<LoadOptionsPaginatedResult<T>>
+
+export type LoadOptions<T> = (input: string) => Promise<T[]>
 
 export interface AutocompleteProps<
   T,
   Multiple extends boolean | undefined = false,
   DisableClearable extends boolean | undefined = false,
-  FreeSolo extends boolean | undefined = false
+  FreeSolo extends boolean | undefined = false,
+  ChipComponent extends React.ElementType = ChipTypeMap['defaultComponent']
 > extends Omit<
-    MuiAutocompleteProps<T, Multiple, DisableClearable, FreeSolo>,
-    | 'onChange'
-    | 'renderInput'
+    MuiAutocompleteProps<T, Multiple, DisableClearable, FreeSolo, ChipComponent>,
     | 'options'
-    | 'clearOnBlur'
-    | 'disableCloseOnSelect'
-    | 'filterSelectedOptions'
-    | 'filterOptions'
-    | 'isOptionEqualToValue'
-    | 'multiple'
-    | 'disableClearable'
-    | 'renderTags'
-    | 'noOptionsText'
-    | 'renderGroup'
+    | 'getOptionLabel'
+    | 'renderOption'
+    | 'onChange'
+    | 'loadingText'
+    | 'loading'
+    | 'open'
+    | 'onOpen'
+    | 'onClose'
+    | 'onInputChange'
+    | 'renderInput'
   > {
   /**
    * Array of options.
    */
   options?: ReadonlyArray<T>
   /**
-   * Callback fired when the value changes.
-   * @param {T|T[]} value The new value of the component.
-   * @param {React.SyntheticEvent} event The event source of the callback.
+   * Used to determine the string value for a given option.
+   * It's used to fill the input (and the list box options if renderOption is not provided).
+   * If used in free solo mode, it must accept both the type of the options and a string.
+   * @param {T} option The option to render.
+   * @returns {string} The string value of the option that should be displayed.
    */
-  onChange?: (
-    value: AutocompleteValue<T, Multiple, DisableClearable, FreeSolo>,
-    event?: React.SyntheticEvent,
-    reason?: AutocompleteChangeReason
-  ) => void
+  getOptionLabel?: (option: T) => string
   /**
-   * The content of the helper under the input.
+   * @default "id"
+   * The key of values from options.
    */
-  helperText?: React.ReactNode
+  valueKey?: string | ((option: T) => string)
   /**
-   * Text to be displayed as a placeholder in the text field.
+   * @default "name"
+   * Which property of our option object will be displayed as label.
    */
-  placeholder?: string
+  labelKey?: string | ((option: T) => string)
   /**
    * @default false
    * If true, the user can select multiple values from list.
@@ -85,81 +95,29 @@ export interface AutocompleteProps<
   isMultiSelection?: boolean
   /**
    * @default false
+   * If true, the options list will have checkboxes.
+   */
+  withCheckboxes?: boolean
+  /**
+   * @default false
    * If true, the user can clear the selected value.
    */
   isClearable?: boolean
   /**
-   * @default true
-   * If false, the user cannot type in Autocomplete, filter options or create new ones.
+   * Render the option, use `getOptionLabel` by default.
+   *
+   * @param {object} props The props to apply on the li element.
+   * @param {T} option The option to render.
+   * @param {object} state The state of each option.
+   * @param {object} ownerState The state of the Autocomplete component.
+   * @returns {ReactNode}
    */
-  isSearchable?: boolean
-  /**
-   * @default false
-   * If true, the value set on change will be set to option[valueKey]/
-   * We use this prop when our options are objects.
-   * If our options are strings, we do not need to send this prop
-   * as this functionality is handled by default.
-   */
-  simpleValue?: boolean
-  /**
-   * Label to be displayed in the heading component.
-   */
-  label?: string
-  /**
-   * @default "id"
-   * The key of values from options.
-   */
-  valueKey?: string
-  /**
-   * @default "name"
-   * Which property of our option object will be displayed as label.
-   */
-  labelKey?: string
-  /**
-   * @default false
-   * If true, the helper text is displayed when an error pops up.
-   */
-  error?: boolean
-  /**
-   * The value of label when a new option is added.
-   */
-  createdLabel?: string
-  /**
-   * Function that returns a promise, which resolves to the set of options to be used once the promise resolves.
-   */
-  loadOptions?: LoadOptions | LoadOptionsPaginated
-  /**
-   * The default set of options to show before the user starts searching. When set to true, the results for loadOptions('') will be autoloaded.
-   */
-  defaultOptions?: boolean | object[]
-  /**
-   * @default "textSecondary"
-   * The color of both the text displayed when there are no options and placeholder. It supports those theme colors that make sense for this component.
-   */
-  typographyContentColor?: TypographyColor
-  /**
-   * Text to display when there are no options.
-   * For localization purposes, you can use the provided translations.
-   */
-  noOptionsText?: React.ReactNode | ((inputValue: string, loadingOptions: boolean) => React.ReactNode)
-  /**
-   * The color of selected input.
-   */
-  inputSelectedColor?: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | string
-  /**
-   *  Properties that will be passed to the rendered input. This is a TextField.
-   */
-  inputTextFieldProps?: Partial<TextFieldProps>
-  /**
-   * If `true`, the label is displayed as required and the `input` element is required.
-   * @default false
-   */
-  required?: boolean
-  /**
-   * @default false
-   * If true, the options list will have checkboxes.
-   */
-  withCheckboxes?: boolean
+  renderOption?: (
+    props: React.HTMLAttributes<HTMLLIElement> & { key: any },
+    option: T,
+    state: AutocompleteRenderOptionState,
+    ownerState: AutocompleteOwnerState<T, Multiple, DisableClearable, FreeSolo, ChipComponent>
+  ) => React.ReactNode
   /**
    * @default false
    * If true, the Autocomplete is free solo, meaning that the user input is not bound to provided options and can add
@@ -167,27 +125,110 @@ export interface AutocompleteProps<
    */
   creatable?: boolean
   /**
-   * Render the input.
-   *
-   * @param {object} params
-   * @returns {ReactNode}
+   * The value of label when a new option is added.
    */
-  renderInput?: (params: AutocompleteRenderInputParams) => React.ReactNode
+  createdLabel?: string
+  /**
+   * Callback fired when the value changes.
+   *
+   * @param {React.SyntheticEvent} event The event source of the callback.
+   * @param {T|T[]} value The new value of the component.
+   * @param {string} reason One of "createOption", "selectOption", "removeOption", "blur" or "clear".
+   * @param {string} [details]
+   */
+  onChange?: (
+    value: AutocompleteValue<T, Multiple, DisableClearable, FreeSolo>,
+    event: React.SyntheticEvent,
+    reason: AutocompleteChangeReason,
+    details?: AutocompleteChangeDetails<T>
+  ) => void
+  /**
+   * Label to be displayed in the heading component.
+   */
+  label?: string
+  /**
+   * Text to be displayed as a placeholder in the text field.
+   */
+  placeholder?: string
+  /**
+   * @default false
+   * If true, the helper text is displayed when an error pops up.
+   */
+  error?: boolean
+  /**
+   * The content of the helper under the input.
+   */
+  helperText?: React.ReactNode
+  /**
+   * If `true`, the label is displayed as required and the `input` element is required.
+   * @default false
+   */
+  required?: boolean
+  /**
+   * @default true
+   * If false, the user cannot type in Autocomplete, filter options or create new ones.
+   */
+  isSearchable?: boolean
+  /**
+   *  Properties that will be passed to the rendered input. This is a TextField.
+   */
+  inputTextFieldProps?: Partial<TextFieldProps>
+  /**
+   * Text to display when in a loading state.
+   *
+   * For localization purposes, you can use the provided [translations](https://mui.com/material-ui/guides/localization/).
+   * @default 'Loading…'
+   */
+  loadingText?: React.ReactNode
+  /**
+   * If `true`, the component is in a loading state.
+   * This shows the `loadingText` in place of suggestions (only if there are no suggestions to show, for example `options` are empty).
+   * @default false
+   */
+  loading?: boolean
+  /**
+   * Function that returns a promise, which resolves to the set of options to be used once the promise resolves.
+   */
+  loadOptions?: LoadOptions<T> | LoadOptionsPaginated<T>
+  /**
+   * If `true`, the component is shown.
+   */
+  open?: boolean
+  /**
+   * Callback fired when the popup requests to be opened.
+   * Use in controlled mode (see open).
+   *
+   * @param {React.SyntheticEvent} event The event source of the callback.
+   * @returns {void}
+   */
+  onOpen?: (event: React.SyntheticEvent) => void
+  /**
+   * Callback fired when the popup requests to be closed.
+   * Use in controlled mode (see open).
+   *
+   * @param {React.SyntheticEvent} event The event source of the callback.
+   * @param {string} reason Can be: `"toggleInput"`, `"escape"`, `"selectOption"`, `"removeOption"`, `"blur"`.
+   */
+  onClose?: (event: React.SyntheticEvent, reason: AutocompleteCloseReason) => void
+  /**
+   * Callback fired when the input value changes.
+   *
+   * @param {React.SyntheticEvent} event The event source of the callback.
+   * @param {string} value The new value of the text input.
+   * @param {string} reason Can be: `"input"` (user input), `"reset"` (programmatic change), `"clear"`, `"blur"`, `"selectOption"`, `"removeOption"`
+   * @returns {void}
+   */
+  onInputChange?: (event: React.SyntheticEvent, value: string, reason: AutocompleteInputChangeReason) => void
   /**
    * @default false
    * If true, the options list will be loaded incrementally using the paginated loadOptions callback
    */
   isPaginated?: boolean
   /**
-   * @default false
-   * Stops click and change event propagation.
-   */
-  stopEventPropagation?: boolean
-  /**
-   * Render the group.
+   * Render the input.
    *
-   * @param {AutocompleteRenderGroupParams} params The group to render.
+   * @param {object} params
    * @returns {ReactNode}
    */
-  renderGroup?: (params: AutocompleteRenderGroupParams) => React.ReactNode
+  renderInput?: (params: AutocompleteRenderInputParams) => React.ReactNode
 }
--- src/components/inputs/Autocomplete/utils.ts
+++ src/components/inputs/Autocomplete/utils.ts
@@ -1,85 +1,15 @@
-import { createFilterOptions } from '@mui/material/Autocomplete'
-import { prop, map, innerJoin, find, propEq, all, includes, is, isEmpty, isNil, props, omit, equals, any } from 'ramda'
-import { AutocompleteValue, FilterOptionsState } from '@mui/material'
+import { emptyString } from 'components/utils/constants'
+import { curry, either, isEmpty, isNil, prop } from 'ramda'
 
-export const findFirstNotNil = (propNames: string[], option: any) => find(x => !isNil(x), props(propNames, option))
-export const isStringOrNumber = (option: unknown) => is(String, option) || is(Number, option)
+export const isNilOrEmpty = either(isNil, isEmpty)
 
-const hasStringOptions = <T>(options: T[]) => all(is(String), options) && !isEmpty(options)
-
-const filter = createFilterOptions()
-export const filterOptions =
-  (labelKey: string, valueKey: string, creatable: boolean) => (options: unknown[], params: FilterOptionsState<unknown>) => {
-    const filtered = filter(options, params)
-    const { inputValue } = params
-
-    // Suggest the creation of a new value if it's not empty and it doesn't already exist
-    const exists = any(
-      (option: any) =>
-        is(Object, option)
-          ? equals(inputValue, is(String, option?.[labelKey]) ? option?.[labelKey] : JSON.stringify(option?.[labelKey]))
-          : equals(inputValue, is(String, option) ? option : JSON.stringify(option)),
-      options
-    )
-
-    if (creatable && !(isEmpty(inputValue) || isNil(inputValue)) && !exists) {
-      filtered.push(
-        hasStringOptions(options)
-          ? {
-              _primitiveValue: inputValue,
-              _createdOption: true
-            }
-          : {
-              // TODO: The valueKey should be different from the inputValue
-              [valueKey]: inputValue,
-              [labelKey]: inputValue,
-              _createdOption: true
-            }
-      )
-    }
-
-    return filtered
+export const extractFirstValue = curry((fns, obj) => {
+  for (let i = 0; i < fns.length; ++i) {
+    const result = fns[i](obj)
+    if (!isNilOrEmpty(result)) return result.toString()
   }
+  return emptyString
+})
 
-export const getSimpleValue = <T extends Record<string, unknown>>(
-  readonlyOptions: readonly T[],
-  value: unknown,
-  valueKey: string,
-  isMultiSelection: boolean
-) => {
-  const options = readonlyOptions as T[]
-  if (isMultiSelection && (!is(Array, value) || isEmpty(options))) return []
-  if (!all(is(Object), options)) return value
-
-  // Add new options if the Autocomplete is multiSelection and creatable
-  if (is(Array, value)) {
-    const optionsSimpleValues = map(prop(valueKey), options)
-    value?.map(v => {
-      if (!includes(v, optionsSimpleValues)) options.push({ [valueKey]: v } as T)
-    })
-  }
-
-  const result = isMultiSelection
-    ? innerJoin((o, v) => o[valueKey as keyof T] === v, options, value as any)
-    : find(propEq(value, valueKey), options)
-  return result || null
-}
-
-export const computeChangedMultiValue = <T, Multiple, DisableClearable, FreeSolo>(
-  input: any,
-  simpleValue: boolean,
-  valueKey: string,
-  labelKey: string
-): AutocompleteValue<T, Multiple, DisableClearable, FreeSolo> =>
-  simpleValue
-    ? input.map((a: unknown) => (!is(Object, a) ? a : findFirstNotNil([valueKey, labelKey, '_primitiveValue'], a)))
-    : input.map((a: unknown) => (!is(Object, a) ? a : prop('_primitiveValue', a) || omit(['_createdOption'], a)))
-
-export const computeChangedSingleValue = (input: any, simpleValue: boolean, valueKey: string, labelKey: string) =>
-  simpleValue
-    ? findFirstNotNil([valueKey, labelKey], input)
-    : (prop('_primitiveValue', input) ?? omit(['_createdOption'], input))
-
-export const stopPropagation = (event: any) => {
-  event.stopPropagation()
-}
+export const internalLabel = prop('__internalDisplay')
+export const internalValue = prop('__internalInputValue')
