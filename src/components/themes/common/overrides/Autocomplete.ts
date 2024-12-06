ee361ee89c0e9c1c1d5319fb8d2f3a7163d6f960
Andra <andarbuz@gmail.com>
2024-12-06 14:29:05 +0200

    Refactor Autocomplete component imports and improve style overrides

--- src/components/themes/common/overrides/Autocomplete.ts
+++ src/components/themes/common/overrides/Autocomplete.ts
@@ -5,21 +5,22 @@ export default function Autocomplete(theme: Theme): CustomComponents {
   return {
     MuiAutocomplete: {
       styleOverrides: {
-        inputRoot: {
-          '& .MuiAutocomplete-input': {
-            width: 'fit-content',
-            minWidth: 'fit-content'
-          }
-        },
+        inputRoot: (props: any) =>
+          !props.ownerState.multiple && {
+            '& .MuiAutocomplete-input': {
+              width: 'fit-content',
+              minWidth: 'fit-content'
+            }
+          },
         input: {
           display: 'flex',
           whiteSpace: 'nowrap'
         },
         noOptions: {
-          ...theme.typography.defaultFont,
-          padding: `${theme.spacing()}px ${theme.spacing(2)}px`,
-          color: 'textSecondary'
-        },
+            ...theme.typography.defaultFont,
+            padding: `${theme.spacing()}px ${theme.spacing(2)}px`,
+            color: 'textSecondary'
+          },
         option: {
           div: {
             ...theme.typography.defaultFont,
