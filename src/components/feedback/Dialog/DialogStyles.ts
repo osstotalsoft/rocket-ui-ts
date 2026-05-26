e5ae095be974f961efe733dfb3304acff5125977
Andra <andarbuz@gmail.com>
2026-05-26 11:10:00 +0300

    Fixed Dialog close button position

--- src/components/feedback/Dialog/DialogStyles.ts
+++ src/components/feedback/Dialog/DialogStyles.ts
@@ -3,21 +3,17 @@ import MuiDialogTitle from '@mui/material/DialogTitle'
 import Backdrop from '@mui/material/Backdrop'
 import { styled } from '@mui/material/styles'
 
-export const justifyRight = {
-  position: 'absolute',
-  top: 'auto',
-  right: 14
-}
-
 export const DialogContent = styled(MuiDialogContent)(({ theme }) => ({
   fontFamily: theme.typography.defaultFont.fontFamily,
   fontSize: theme.typography.defaultFont.fontSize
 }))
 
-export const DialogTitle = styled(MuiDialogTitle)(({ theme }) => ({
-  fontFamily: theme.typography.defaultFont.fontFamily,
-  padding: 0
-}))
+  export const DialogTitle = styled(MuiDialogTitle)(({theme }) => ({
+    fontFamily: theme.typography.defaultFont.fontFamily,
+    flex: 1,
+    minWidth: 0,
+    padding: 0
+  }))
 
 export const TransparentBackdrop = styled(Backdrop)(() => ({
   backgroundColor: 'transparent'
